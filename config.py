903a0282ac3ca0651a21f69afcba6b27ca6fc5b3
johyemtol <cho26739830@gmail.com>
2024-11-21 18:35:36 +0900

    feat: 응급콜, 자가진단 연결

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/semi-capstone'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:waa101704!@localhost:3306/semi-capstone'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- views/views.py
+++ views/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from urllib import request
+from flask import Blueprint,render_template, request, url_for, redirect
 
 from semi.views.user_view import login_required
 
@@ -34,3 +35,44 @@ def result():
 @app.route('/call')
 def call():
     return render_template('call.html')
+
+@app.route('/depression')
+def depression():
+    return render_template('depression.html')
+
+@app.route('/bipolar')
+def bipolar():
+    return render_template('bipolar.html')
+
+@app.route('/ptsd')
+def ptsd():
+    return render_template('ptsd.html')
+
+@app.route('/adhd')
+def adhd():
+    return render_template('adhd.html')
+
+@app.route('/anxiety')
+def anxiety():
+    return render_template('anxiety.html')
+
+# 우울증 자가진단 결과 처리
+@app.route('/submit_depression', methods=['POST'])
+def submit_depression():
+    total_score = 0
+    for i in range(1, 21):  # 질문 1부터 20까지
+        question_value = request.form.get(f'question{i}')
+        if question_value:
+            total_score += int(question_value)
+
+    # 점수 25점 이상이면 응급콜 페이지로 리디렉션
+    if total_score >= 25:
+        return redirect(url_for('app_bp.emergency_call'))  # 응급콜 페이지로 리디렉션
+    else:
+        result = f"우울증 가능성 점수: {total_score}. 추가적인 조치를 취하는 것이 좋습니다."
+        return render_template('result.html', result=result)
+
+# 정신건강 위기상담 페이지 (emergency_call.html)
+@app.route('/emergency_call')
+def emergency_call():
+    return render_template('emergency_call.html')
