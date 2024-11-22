28b6e106df3889f5cc15168876bb5fa5e2157188
johyemtol <cho26739830@gmail.com>
2024-11-22 19:44:18 +0900

    가까운 병원 서비스 추가

--- views/views.py
+++ views/views.py
@@ -51,23 +51,11 @@ def adhd():
 def anxiety():
     return render_template('anxiety.html')
 
-# 우울증 자가진단 결과 처리
-@app.route('/submit_depression', methods=['POST'])
-def submit_depression():
-    total_score = 0
-    for i in range(1, 21):  # 질문 1부터 20까지
-        question_value = request.form.get(f'question{i}')
-        if question_value:
-            total_score += int(question_value)
-
-    # 점수 25점 이상이면 응급콜 페이지로 리디렉션
-    if total_score >= 25:
-        return redirect(url_for('app_bp.emergency_call'))  # 응급콜 페이지로 리디렉션
-    else:
-        result = f"우울증 가능성 점수: {total_score}. 추가적인 조치를 취하는 것이 좋습니다."
-        return render_template('result.html', result=result)
-
 # 정신건강 위기상담 페이지 (emergency_call.html)
 @app.route('/emergency_call')
 def emergency_call():
     return render_template('emergency_call.html')
+
+@app.route('/hospital')
+def hospital():
+    return render_template('hospital.html')
